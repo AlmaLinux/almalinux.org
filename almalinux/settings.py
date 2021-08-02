828f95c27b5761e936438b47b9fc048d5a15e268
Matīss Treinis <matiss@hub256.com>
2021-08-02 22:30:34 +0200

    Enable Chinese (Simplified)

--- almalinux/settings.py
+++ almalinux/settings.py
@@ -160,7 +160,7 @@ LANGUAGES = (
     ('ru', 'Русский'),
     ('tr', 'Türkçe'),
     ('uk', 'Украї́нська'),
-    # ('zh_Hans', 'Chinese (Simplified)'),
+    ('zh_Hans', 'Chinese (Simplified)'),
 )
 
 LOCALE_PATHS = [
