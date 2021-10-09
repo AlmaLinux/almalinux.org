ac5182d85952e6e950f5d99db442a099643531f1
Matīss Treinis <matiss@hub256.com>
2021-10-09 17:16:30 +0200

    Change media permalink base

--- www/urls.py
+++ www/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('contribute/', views.contribute_index, name='contribute_index'),
     path('foundation/members/', views.foundation_members, name='foundation_members'),
     path('feed/', BlogPostFeed(), name='feed'),
-    path('media/<int:media_id>', views.media_element, name='media_element'),
+    path('media-link/<int:media_id>', views.media_element, name='media_element'),
 ]
